2bffca0ea74cbcce61b3f3fb0a3e10401fbf7958
Aditya Patwal <adityapatwal07@gmail.com>
2025-12-14 13:35:33 -0500

    Day 1

---
+++ day1.py
@@ -0,0 +1,60 @@
+'''
+Day 1 advent of code
+
+dial starts at 50
+
+L goes toward lower numbers -> CCW
+R goes toward higher numbers -> CW
+
+0,1,2... 50.... 98,99,0,1
+
+          99 0 1
+        75      25
+            50
+
+            
+count exactly how many times the dial points at 0
+
+'''
+
+input = open('input.txt').read().strip()
+
+dial = 50
+count = 0
+iter = -1
+
+for command in input.split():
+    iter+=1
+    direction = command[0]
+    steps = int(command[1:])
+
+    if steps > 100: # mod 100 to reduce unnecessary full rotations
+        steps = steps % 100
+
+    if direction == 'L':
+        '''
+        subtract and check if we hit negative numbers,
+        if we do then subtract the abs val from 100
+        '''
+        if dial - steps < 0:
+            x = abs(dial - steps)
+            dial = 100 - x
+        else:
+            dial -= steps
+
+    elif direction == 'R':
+        '''
+        add and check if we go over 99,
+        if we do then subtract 100 from the new value
+        '''
+        if dial + steps > 99:
+            dial = (dial + steps) - 100
+        else:
+            dial += steps
+    
+    # print(direction, steps, dial)
+
+    if dial == 0:
+        count += 1
+        
+print(count)
