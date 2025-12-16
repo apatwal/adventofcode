1cc444ae80af03cb9940048893f129280dd753c2
Aditya Patwal <adityapatwal07@gmail.com>
2025-12-15 23:42:28 -0500

    d2p2

--- d2/day2.py
+++ d2/day2.py
@@ -15,9 +15,25 @@ for id in input.split(','):
         if(isValid(str(i))):
             answer += i
 
+''' Day 2 Part 2'''
+def isValid2(num):
+    doubled = num + num
+    return True if num in doubled[1:-1] else False
+
+
+answer = 0
+    
+for id in input.split(','):
+    l, r = map(int, id.split('-'))
+    for i in range(l, r+1):
+        if(isValid2(str(i))):
+            answer += i
 
 print(answer)
 # Test
-# print(isValid("123123"))  # True
-# print(isValid("55"))  # True
-# print(isValid("101"))  # False
+print(isValid("123123"))  # True
+print(isValid("55"))  # True
+print(isValid("101"))  # False
+print(isValid2("111"))  # True
+print(isValid2("121"))  # False
+print(isValid2("1212"))  # True
