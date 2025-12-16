402655fa3f6ba619d7b1a9ea5855e574c08e8e3e
Aditya Patwal <adityapatwal07@gmail.com>
2025-12-16 00:15:21 -0500

    Optimizing solution

--- d3/day3.py
+++ d3/day3.py
@@ -2,27 +2,26 @@ ans = 0
 
 input = open('d3/input.txt').read().strip()
 lines = input.split('\n')
-for bank in lines:
-    batteries = int(bank) # int representation of the battery bank
-    l, r = 0, len(bank) - 1
-    lmax, rmax = 0, 0
-    while l < r:
-        if int(bank[l]) > int(bank[r]):
-            rmax = max(rmax, int(bank[r]))
-            lmax = max(lmax, int(bank[l]))
-            r-=1
-        elif int(bank[l]) <= int(bank[r]):
-            lmax = max(lmax, int(bank[l]))
-            rmax = max(rmax, int(bank[r]))
-            l+=1
 
-    if rmax > lmax and r+1 in range(len(bank)):
-        # set lmax to rmax and then find largest right
-        lmax = rmax
-        rmax = 0
-        for i in range(r+1, len(bank)):
-            rmax = max(rmax, int(bank[i]))
-    maxbat = str(lmax) + str(rmax)
-    ans += int(maxbat)
+'''
+better approach. find the largest battery from battery[0] to battery[i-1]. this would be lmax
+find the largest battery from battery[i+1] to battery[n-1]. this would be
+'''
+for bank in lines:
+    lmax = 0
+    index = 0
+    for i in range(0, len(bank)-1):
+        if int(bank[i]) > lmax:
+            index = i
+            lmax = int(bank[i])
+    
+    rmax = 0
+    for j in range(index+1, len(bank)):
+        if int(bank[j]) > rmax:
+            rmax = int(bank[j])
+
+    battery = str(lmax) + str(rmax)
+    ans += int(battery)
+    
 
 print(ans)
